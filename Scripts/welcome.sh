6a42250c690f14de011d23204ed12a1a876d616e
tuxfanboy <108594413+tuxfanboy@users.noreply.github.com>
2022-07-22 17:48:59 -0400

    Add files via upload
    Added welcome.sh

---
+++ Scripts/welcome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cat $HOME/Documents/welcome.txt | lolcat;
+      tput setaf 6; cat $HOME/Documents/arch.txt; tput sgr0;
+  echo " "; echo " "; cat $HOME/Documents/text.txt;
+  tput setaf 3; printf '  pacman -Syu'; tput sgr0; printf " as root.";
+    echo " "; echo " "
